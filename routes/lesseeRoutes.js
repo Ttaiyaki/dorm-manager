3f910b739c605c1efa2b473f348f26a4fecc91b2
Ttaiyaki <66070325@kmitl.ac.th>
2025-03-06 22:27:20 +0700

    add client-payment history page

--- routes/lesseeRoutes.js
+++ routes/lesseeRoutes.js
@@ -27,6 +27,20 @@ router.get('/profile', (req, res) => {
     })
 });
 
+router.get('/payment-history', (req, res) => {
+    // if (!req.cookies.user_id) {return res.redirect('/log-in');}
+    // res.render('lessee/payment-history-page');
+    lesseeService.getUserByID(req.cookies.user_id, (err, user) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        console.log(user)
+        
+        return res.render('lessee/payment-history-page', { data : user });
+    })
+});
+
 router.get('/profile/edit', (req, res) => {
     if (!req.cookies.user_id) {return res.redirect('/log-in');}
     res.render('lessee/profile-edit');
