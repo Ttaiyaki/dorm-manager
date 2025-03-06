4e29964f7c0432f3832266676c94806134ed19b8
Takku1N <lightroj00@gmail.com>
2025-03-07 01:07:06 +0700

    "Show db in clientHome"

--- routes/lesseeRoutes.js
+++ routes/lesseeRoutes.js
@@ -15,13 +15,37 @@ router.get('/', (req, res) => {
         return res.redirect('/log-in');
     }
     if (req.cookies.user.user_status != "Verify") {return res.send("User's not verified.");}
-    lesseeService.getUserByID(req.cookies.user.user_id, (err, user) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
 
-        return res.render('lessee/clientHome');
+    
+    const getUserRoom = ` SELECT room_id FROM users WHERE user_id = ?; `;
+    lesseeService.getData(getUserRoom, [req.cookies.user.user_id], (err, room_id) => {
+        if (err) {return console.log(err.message);}
+        console.log(room_id[0].room_id);
+        
+        const getRoom = ` SELECT * FROM rooms r
+                      INNER JOIN room_facilities rf USING (room_id)
+                      INNER JOIN facilities f USING (facility_id)
+                      WHERE room_id = ?; `;
+        lesseeService.getData(getRoom, [room_id[0].room_id], (err, room) => {
+            if (err) {return console.log(err.message);}
+            console.log(room);
+
+            const getBill = ` SELECT * FROM bills WHERE user_id = ? ORDER BY bill_id DESC LIMIT 1 `;
+            lesseeService.getData(getBill, [req.cookies.user.user_id], (err, bill) => {
+                if (err) {return console.log(err.message);}
+                
+                console.log(bill);
+                return res.render('lessee/clientHome', {room : room, bill : bill})
+            })
+        })
     })
+    // lesseeService.getUserByID(req.cookies.user.user_id, (err, user) => {
+    //     if (err) {
+    //         return res.status(500).json({ error: err.message });
+    //     }
+
+    //     return res.render('lessee/clientHome', {user : req.cookies.user});
+    // })
 });
 
 router.get('/profile', (req, res) => {
