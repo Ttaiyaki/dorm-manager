06b60b167c99eedd06babf38b8b7873da2ff1e36
Takku1N <lightroj00@gmail.com>
2025-03-08 06:26:11 +0700

    clientHome fix error & connect clientHome - profile to db

--- routes/lesseeRoutes.js
+++ routes/lesseeRoutes.js
@@ -11,15 +11,24 @@ router.get('/', (req, res) => {
     console.log(req.cookies.user);
     if (!req.cookies.user) {
         res.clearCookie('user');
-        req.session.popup = "not login"
+        req.session.popup = "not login";
         return res.redirect('/log-in');
     }
-    if (req.cookies.user.user_status != "Verify") {return res.send("User's not verified.");}
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
 
-    
     const getUserRoom = ` SELECT room_id FROM users WHERE user_id = ?; `;
     lesseeService.getData(getUserRoom, [req.cookies.user.user_id], (err, room_id) => {
         if (err) {return console.log(err.message);}
+        if (room_id.length === 0) {
+            return 
+        }
         console.log(room_id[0].room_id);
         
         const getRoom = ` SELECT * FROM rooms r
@@ -28,42 +37,86 @@ router.get('/', (req, res) => {
                       WHERE room_id = ?; `;
         lesseeService.getData(getRoom, [room_id[0].room_id], (err, room) => {
             if (err) {return console.log(err.message);}
+            if (room.length === 0) {console.log('no room')}
             console.log(room);
 
             const getBill = ` SELECT * FROM bills WHERE user_id = ? ORDER BY bill_id DESC LIMIT 1 `;
             lesseeService.getData(getBill, [req.cookies.user.user_id], (err, bill) => {
                 if (err) {return console.log(err.message);}
-                
-                console.log(bill);
-                return res.render('lessee/clientHome', {room : room, bill : bill})
+                if (bill.length === 0) {console.log('no bill')}
+                else {
+                    console.log(bill);
+                    const date = new Date(bill[0].month);
+                    console.log(date)
+                    const monthName = date.toLocaleDateString('th-Th', { month: 'long' }); // แปลงเดือนเป็นชื่อเดือนไทย
+                    bill[0].month = monthName;
+
+                    bill[0].total = bill[0].elect_bill + bill[0].water_bill // รวมค่าน้ำค่าไฟ
+                    bill[0].due_date = new Date(bill[0].due_date).toLocaleDateString('th-Th', { day: "numeric", month: "long", year: "numeric" });
+                }
+
+                const getMeter = `SELECT * FROM meters WHERE room_id = ? ORDER BY meter_id DESC LIMIT 1`;
+                lesseeService.getData(getMeter, [room_id[0].room_id], (err, meter) => {
+                    if (err) {return console.log(err);}
+                    if (meter.length === 0) {console.log('no meter')}
+                    console.log(meter)
+                    return res.render('lessee/clientHome', {room : room, bill : bill, meter : meter})
+                })
             })
         })
     })
-    // lesseeService.getUserByID(req.cookies.user.user_id, (err, user) => {
-    //     if (err) {
-    //         return res.status(500).json({ error: err.message });
-    //     }
-
-    //     return res.render('lessee/clientHome', {user : req.cookies.user});
-    // })
 });
 
 router.get('/profile', (req, res) => {
-    if (!req.cookies.user) {return res.redirect('/log-in');}
-    console.log(req.cookies);
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
     lesseeService.getUserByID(req.cookies.user.user_id, (err, user) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
+        if (user.length === 0) {console.log('no user')}
 
         console.log(user)
-        return res.render('lessee/profile', { data : user });
+        
+        const getRoom = `SELECT * FROM rooms WHERE room_id = ?;`;
+        lesseeService.getData(getRoom, [user.room_id], (err, room) => {
+            if (err) {return console.log(err.message)}
+            if (room.length === 0) {console.log('no room');}
+
+            console.log(room[0])
+            return res.render('lessee/profile', { user : user, room : room[0] });
+        })
+        
     })
 });
 
 router.get('/payment-history', (req, res) => {
-    // if (!req.cookies.user_id) {return res.redirect('/log-in');}
-    // res.render('lessee/payment-history-page');
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
     lesseeService.getUserByID(req.cookies.user_id, (err, user) => {
         if (err) {
             return res.status(500).json({ error: err.message });
@@ -76,19 +129,58 @@ router.get('/payment-history', (req, res) => {
 });
 
 router.get('/profile/edit', (req, res) => {
-    if (!req.cookies.user) {return res.redirect('/log-in');}
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
     res.render('lessee/profile-edit');
 });
 
 router.get('/uploadConfirmation', (req, res) => {
-    if (!req.cookies.user.user_id) { return res.redirect('/log-in'); }
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
     const status = req.cookies.user.user_status;
 
     res.render('lessee/uploadConfirmation', { status });
 });
 
 router.post('/upload', upload.single('payment_slip'), (req, res) => {
-
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
     const billId = req.cookies.bill_id;
     const fileBuffer = req.file.buffer;
 
@@ -98,7 +190,20 @@ router.post('/upload', upload.single('payment_slip'), (req, res) => {
 });
 
 router.post('/profile/update', upload.single('user_img'), (req, res) => {
-    if (!req.cookies.user.user_id) { return res.redirect('/log-in'); }
+    console.log(req.cookies.user);
+    if (!req.cookies.user) {
+        res.clearCookie('user');
+        req.session.popup = "not login";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_type != 1) {
+        req.cookies.popup = "not lessee";
+        return res.redirect('/log-in');
+    }
+    if (req.cookies.user.user_status != "Verify") {
+        req.session.popup = "user not verify";
+        return res.redirect('/log-in');
+    }
 
     const userId = req.cookies.user.user_id;
     const { first_name, last_name, email, phone, user_name } = req.body;
@@ -113,16 +218,3 @@ router.post('/profile/update', upload.single('user_img'), (req, res) => {
 });
 
 module.exports = router;
-
-
-// app.get('/', (req, res) => {
-//     res.render('clientHome');
-//   });
-  
-// app.get('/profile', (req, res) => {
-//     res.render('profile');
-// })
-
-// app.get('/profile/edit', (req, res) => {
-//     res.render('profile-edit');
-// })
